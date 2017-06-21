3694efdf3d340a7a4ca6c9332b8be56953aa69c0
tnt2677 <tiwaritanishk26@gmail.com>
2017-06-21 20:12:36 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+typedef  long long ll;
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m,d;
+        cin>>n>>m>>d;
+        priority_queue<int> pq;
+        for(int i=0;i<n;++i){
+            int x;
+            cin>>x;
+            pq.push(x);
+        }
+        for(int i=0;i<m;++i){
+            int max_value=pq.top();pq.pop();
+            max_value-=d;
+            if(max_value>0)
+                pq.push(max_value);
+        }
+        if(pq.size()==n)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+    return 0;
+}
