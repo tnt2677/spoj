61a43fd9ab36b06d9ca022fc5273d2ae62ec620c
tnt2677 <tiwaritanishk26@gmail.com>
2017-07-24 13:58:33 +0530

    Update can_you_answer_these_queris_1.cpp

--- can_you_answer_these_queris_1.cpp
+++ can_you_answer_these_queris_1.cpp
@@ -4,28 +4,48 @@ typedef int long long ll;
 
 using namespace std;
 
+//maximum subarray sum can be calculated via:
+//prefix sum,suffix sum,total sum
+//ie. 
 struct node{
     ll prefix,suffix,sum,max_sum;
 };
+//builds the tree: needs parameters node_num,range for which current node is responsible
+//ie. node_num: responsible for [lo....hi]
 void build(int lo ,int hi, int node_num,int arr[],node seg_tree[]){
+    //if current node is leaf: ie. lo=hi
+    //copy the data from array to the segment tree
     if(lo==hi){
       seg_tree[node_num].sum = arr[lo];
       seg_tree[node_num].prefix = arr[lo];
       seg_tree[node_num].suffix = arr[lo];
       seg_tree[node_num].max_sum = arr[lo];
+      //then return
       return;
     }
+    //else 
+    //calculate first build the childs recursively 
     int mid = (lo+hi)/2;
     build(lo,mid,node_num*2,arr,seg_tree);
     build(mid+1,hi,node_num*2+1,arr,seg_tree);
+    //node left: contains the data of left child
     node left=seg_tree[node_num*2];
+    //node right : contains the data of right child
     node right=seg_tree[node_num*2+1];
+    //now using the data from the left & right childs fill in the data of current node
+    //sum: left.sum+right.sum
     seg_tree[node_num].sum = left.sum + right.sum;
+    //max-prefix: maximum of left.prefix & left.sum + right.prefix 
     seg_tree[node_num].prefix = max( left.prefix , left.sum + right.prefix);
+    //max-suffix: maximum of right.suffix & right.sum + left.suffix
     seg_tree[node_num].suffix = max( right.suffix , right.sum + left.suffix);
+    //max-sum: either the max-sum can be contained entirely in left node or right node
+    //or partially in both: hence left.suffix + right.prefix
     seg_tree[node_num].max_sum = max( max( left.max_sum , right.max_sum ) , left.suffix + right.prefix );
 }
+//null node for:
 node null;
+// returns the 
 node query(int l,int r,int s,int e,int node_num,node seg_tree[]){
     if(s>r || e<l){
         return null;
