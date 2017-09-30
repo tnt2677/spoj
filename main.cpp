30b12b864cb6ba0764106b19693b7506a5aa7be3
tnt2677 <tiwaritanishk26@gmail.com>
2017-09-30 22:30:44 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,39 +1,65 @@
 #include <bits/stdc++.h>
+#define MAX 50005
+#define ii pair<int,int>
+#define vi vector<int>
+#define vii vector<pair<int,int>>
+#define inf 10000000LL
+typedef long long ll;
 using namespace std;
-//ref:https://www.quora.com/How-can-the-problem-Ones-and-zeros-on-SPOJ-be-solved-using-BFS#
-map<int,string> mp;
-//returns modulos in biginteger
-int mod(const string & str,int n){
-    int ans=0;
-    for(int i=0;i<(int)str.size();++i){
-        ans=(ans*10+(str[i]-'0'))%n;
+struct node{
+    int index;
+    ii ra;
+};
+bool cmp(const node &lhs,const node &rhs){
+    return lhs.ra<rhs.ra;
+}
+node arr[MAX];int temp[MAX];
+void func(char *str,int n){
+    for(int i=0;i<n;++i){
+        arr[i].index=i;
+        arr[i].ra.first=str[i];
+        arr[i].ra.second=(i+1<n)?str[i+1]:-1;
+    }
+    sort(arr,arr+n,cmp);
+    for(int k=2;k<n;k<<=1){
+        int rank=0,prev=arr[0].ra.first;
+        arr[0].ra.first=rank;temp[arr[0].index]=0;
+        for(int i=1;i<n;++i){
+            if(arr[i].ra.first==prev&&arr[i].ra.second==arr[i-1].ra.second){
+                prev=arr[i].ra.first;
+                arr[i].ra.first=rank;
+            }else{
+                prev=arr[i].ra.first;
+                arr[i].ra.first=++rank;
+            }
+            temp[arr[i].index]=i;
+        }
+        for(int i=0;i<n;++i){
+            int nxtrnk=k+arr[i].index;
+            arr[i].ra.second=nxtrnk<n?arr[temp[nxtrnk]].ra.first:-1;
+        }
+        sort(arr,arr+n,cmp);
+    }
+}
+int lcp[MAX];
+ll calculate(char* str ,int n){
+    ll ans=0;
+    for(int i=0,k=0;i<n;++i,k?k--:0){
+        if(temp[i]==n-1){k=0;continue;}
+        int j=arr[temp[i]+1].index;
+        while(i+k<n&&j+k<n&&str[i+k]==str[j+k])k++;
+        lcp[temp[i]]=k;ans+=k;
     }
     return ans;
 }
-//returns first no. containing only of ones and zero's
-//say str%n gives "i"
-//if i has been seen earlier then ignore as it can't possibly give the right answer
-//else mark current stste as visited
-string func(int n){
-    queue<string> q;q.push("1");
-    mp.clear();
-    while(true){
-        string top=q.front();q.pop();
-        int r=mod(top,n);
-        if(!r)return top;
-        if(mp.find(r)==mp.end()){
-            q.push(top+'0');q.push(top+'1');
-            mp[r]=top;
-        }
-    }
-}
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;cin>>t;
+    int t;scanf("%d",&t);
     while(t--){
-        int n;cin>>n;
-        cout<<func(n)<<"\n";
+        char str[MAX];
+        scanf("%s",str);int n=strlen(str);
+        func(str,n);ll x=calculate(str,n);
+        ll ans=((n*(n+1))/2LL)-x;
+        printf("%lld\n",ans);
     }
-    return 0;
+	return 0;
 }
